d81c957d68376a02ddad181ac0685f59e9ff68e4
ankit-hindustani <namaste.ankitsingh@gmail.com>
2020-11-25 22:28:18 +0530

    fix

---
+++ src/components/questions/questionNameList.js
@@ -0,0 +1,11 @@
+
+import FirstQuestion from './firstQuestion';
+import SecondQuestion from './secondQuestion';
+import ThirdQuestion from './thirdQuestion';
+const questionNameList=[
+FirstQuestion,
+SecondQuestion,
+ThirdQuestion
+];
+
+export default questionNameList;
